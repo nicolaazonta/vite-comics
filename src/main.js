4b439a7bb88772339d057a6649ae63b3b81a5f83
nicolaazonta <nicolazonta114@gmail.com>
2023-04-13 16:55:27 +0200

    install and import bootstrap and img

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+// Import all of Bootstrap's CSS
+import "../node_modules/bootstrap/scss/bootstrap.scss";
 createApp(App).mount('#app')
