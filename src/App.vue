f207d91a9c1785a2c7f99c63754a956224d98d79
nicolaazonta <nicolazonta114@gmail.com>
2023-04-13 17:23:12 +0200

    trying to import font

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,29 @@
 
 
 <template>
-  <h1 class="container">dai che ndemo</h1>  
+  <header>
+    <div class="container d-flex justify-content-between">
+      <div class="left_group">
+        <div class="logo">
+          <img class="img-fluid" src="./assets/img/dc-logo.png" alt="">
+        </div>
+      </div>
+      <div class="right_group">
+          <ul class="d-flex text-uppercase list-style-none">
+            <li>characters</li>
+            <li>comics</li>
+            <li>movies</li>
+            <li>tv</li>
+            <li>games</li>
+            <li>collectibles</li>
+            <li>videos</li>
+            <li>fans</li>
+            <li>news</li>
+            <li>shop</li>
+          </ul>
+      </div>
+    </div>
+  </header>  
 <!-- 
   <siteHeader></siteHeader>
   <siteMain></siteMain>
@@ -25,4 +47,8 @@
 
 
 <style>
+*{
+  list-style: none;
+  font-family: 'Oswald', sans-serif;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 // Import all of Bootstrap's CSS
+import ('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
+
+
 import "../node_modules/bootstrap/scss/bootstrap.scss";
 createApp(App).mount('#app')
