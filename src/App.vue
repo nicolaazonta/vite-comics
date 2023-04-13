ae88b983a24e767b253e54c87d1b857ec34eaf9a
nicolaazonta <nicolazonta114@gmail.com>
2023-04-13 21:18:57 +0200

    schematize main section

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@
 
 <template>
   <header>
+    
     <div class="container d-flex justify-content-between align-items-center">
       <div class="left_group">
         <div class="logo">
@@ -24,21 +25,90 @@
       </div>
           <ul class="right_group d-flex align-items-center text-uppercase list-style-none">
             <li class="active">
-              <span class="self-align-center">characters</span>
-              <div class="line_bottom justify-self-end"></div>  
+              <span class="">characters</span>
+              <div class="line_bottom"></div>  
             </li>
-            <li class="px-1">comics</li>
-            <li class="px-1">movies</li>
-            <li class="px-1">tv</li>
-            <li class="px-1">games</li>
-            <li class="px-1">collectibles</li>
-            <li class="px-1">videos</li>
-            <li class="px-1">fans</li>
-            <li class="px-1">news</li>
-            <li class="px-1">shop</li>
+            <li class="">
+              <span class="">comics</span>
+              <div class="line_bottom"></div>  
+            </li>
+            <li class="">
+              <span class="">movies</span>
+              <div class="line_bottom"></div>  
+            </li>
+            <li class="">
+              <span class="">tv</span>
+              <div class="line_bottom"></div>  
+            </li>
+            <li class="">
+              <span class="">games</span>
+              <div class="line_bottom"></div>  
+            </li>
+            <li class="">
+              <span class="">collectibles</span>
+              <div class="line_bottom"></div>  
+            </li>
+            <li class="">
+              <span class="">videos</span>
+              <div class="line_bottom"></div>  
+            </li>
+            <li class="">
+              <span class="">fans</span>
+              <div class="line_bottom"></div>  
+            </li>
+            <li class="">
+              <span class="">news</span>
+              <div class="line_bottom"></div>  
+            </li>
+            <li class="">
+              <span class="">shop</span>
+              <div class="line_bottom"></div>  
+            </li>
+           
           </ul>
     </div>
-  </header>  
+  </header> 
+  
+  <main>
+    <div class="primary_section">
+      <div class="container d-flex align-items-center">
+        <h2>---------content here---------</h2>
+      </div>
+    </div>
+
+    <div class="secondary_section ">
+      <div class="container d-flex justify-content-around align-content-center">
+
+        <section class="d-flex align-items-center">
+          <img class="img-fluid" src="./assets/img/buy-comics-digital-comics.png" alt="">
+          <span>digital comics</span>  
+        </section>
+
+        <section class="d-flex align-items-center">
+          <img class="img-fluid" src="./assets/img/buy-comics-merchandise.png" alt="">
+          <span>dc merchandise</span>  
+        </section>
+
+        <section class="d-flex align-items-center">
+          <img class="img-fluid" src="./assets/img/buy-comics-subscriptions.png" alt="">
+          <span>subscription</span>  
+        </section>
+
+        <section class="d-flex align-items-center">
+          <img class="img-fluid" src="./assets/img/buy-comics-shop-locator.png" alt="">
+          <span>comic schop locator</span>  
+        </section>
+
+        <section class="d-flex align-items-center">
+          <img class="img-fluid" src="./assets/img//buy-dc-power-visa.svg" alt="">
+          <span>dc power visa</span>  
+        </section>
+
+      </div>
+     
+      
+    </div>
+  </main>
 <!-- 
   <siteHeader></siteHeader>
   <siteMain></siteMain>
@@ -48,18 +118,25 @@
 
 
 <style>
+/* #region---------header */
 ul>li{
   padding: 0 1rem;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
+li>span{
+  justify-self: center;
+}
+li>div{
+  justify-self: end;
+}
 *{
   list-style: none;
   font-family: 'Oswald', sans-serif;
 }
 header{
-  background-color: rgb(179, 166, 166);
+  background-color: #ffffff;
 }
 .active{
   color: #0282F9;  
@@ -68,5 +145,31 @@ header{
   background-color: #0282F9;
   height: 2px;
   width: 80%;
+}
+/* #endregion---------header */
+
+/* #region---------main */
+main>.primary_section{
+  color: white;
+  background-color: #1C1C1C;
+  height: 8rem;
+}
+main>.secondary_section{
+  background-color: #0282F9;
+  height: 8rem;
+}
+main .secondary_section img{
+  color: white;
+  width: 3rem;
+}
+main .container{
+  height: 100%;
+}
+/* #endregion---------main */
+
+
+
+main{
+  height: 7rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-// Import all of Bootstrap's CSS
-import ('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
 
 
 import "../node_modules/bootstrap/scss/bootstrap.scss";
