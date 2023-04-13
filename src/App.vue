80103cbbf7e035243f8ca0a4c11673c72256acec
nicolaazonta <nicolazonta114@gmail.com>
2023-04-13 17:50:28 +0200

    finishing header

--- src/App.vue
+++ src/App.vue
@@ -16,26 +16,27 @@
 
 <template>
   <header>
-    <div class="container d-flex justify-content-between">
+    <div class="container d-flex justify-content-between align-items-center">
       <div class="left_group">
         <div class="logo">
           <img class="img-fluid" src="./assets/img/dc-logo.png" alt="">
         </div>
       </div>
-      <div class="right_group">
-          <ul class="d-flex text-uppercase list-style-none">
-            <li>characters</li>
-            <li>comics</li>
-            <li>movies</li>
-            <li>tv</li>
-            <li>games</li>
-            <li>collectibles</li>
-            <li>videos</li>
-            <li>fans</li>
-            <li>news</li>
-            <li>shop</li>
+          <ul class="right_group d-flex align-items-center text-uppercase list-style-none">
+            <li class="active">
+              <span class="self-align-center">characters</span>
+              <div class="line_bottom justify-self-end"></div>  
+            </li>
+            <li class="px-1">comics</li>
+            <li class="px-1">movies</li>
+            <li class="px-1">tv</li>
+            <li class="px-1">games</li>
+            <li class="px-1">collectibles</li>
+            <li class="px-1">videos</li>
+            <li class="px-1">fans</li>
+            <li class="px-1">news</li>
+            <li class="px-1">shop</li>
           </ul>
-      </div>
     </div>
   </header>  
 <!-- 
@@ -47,8 +48,25 @@
 
 
 <style>
+ul>li{
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 *{
   list-style: none;
   font-family: 'Oswald', sans-serif;
+}
+header{
+  background-color: rgb(179, 166, 166);
+}
+.active{
+  color: #0282F9;  
+}
+.line_bottom{
+  background-color: #0282F9;
+  height: 2px;
+  width: 80%;
 }
 </style>
