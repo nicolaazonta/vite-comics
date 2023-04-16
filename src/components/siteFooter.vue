485181f05b9308889749022610f6fdd437115bea
nicolaazonta <nicolazonta114@gmail.com>
2023-04-16 17:30:39 +0200

    complete milestone of day-one

--- src/components/siteFooter.vue
+++ src/components/siteFooter.vue
@@ -6,6 +6,67 @@
 </script>
 
 <template>
+
+    <footer>
+
+        <footer-top>
+            <div class="container">
+                <div class="row py-2">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col">
+                                <ul>
+                                    <h5>dc comics</h5>
+                                    <li>prova</li>
+                                    <li>prova</li>
+                                </ul>
+                                <ul>
+                                    <h5>shop</h5>
+                                    <li>prova</li>
+                                    <li>prova</li>
+                                </ul>
+                            </div>
+                            <div class="col">
+                                <ul>
+                                    <h5>dc</h5>
+                                    <li>prova</li>
+                                    <li>prova</li>
+                                </ul>                            
+                            </div>
+                            <div class="col">
+                                <ul>
+                                    <h5>sites</h5>
+                                    <li>prova</li>
+                                    <li>prova</li>
+                                </ul>                          
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer-top>
+
+        <footer-bottom>
+            <div class="py-3 container d-flex justify-content-between">
+
+                <div class="left_pack">
+                    <span class="text-uppercase btn">sign-up now!</span>
+                </div>
+                
+                <div class="right_pack d-flex align-content-center">
+                    <p class="text-uppercase px-1">follow us</p>
+                    <img src="../assets/img/footer-facebook.png" alt="">
+                    <img src="../assets/img/footer-periscope.png" alt="">
+                    <img src="../assets/img/footer-pinterest.png" alt="">
+                    <img src="../assets/img/footer-twitter.png" alt="">
+                    <img src="../assets/img/footer-youtube.png" alt="">
+                </div> 
+
+            </div>
+        </footer-bottom>
+        
+    </footer>
+
 </template>
 
 
--- src/components/siteMain.vue
+++ src/components/siteMain.vue
@@ -6,41 +6,43 @@
 
 
 <template>
-    <div class="primary_section">
-        <div class="container d-flex align-items-center">
-            <h2>---------content here---------</h2>
+    <main>
+        <div class="primary_section">
+            <div class="container d-flex align-items-center">
+                <h2>---------content here---------</h2>
+            </div>
         </div>
-    </div>
-
-    <div class="secondary_section ">
-        <div class="container d-flex justify-content-around align-content-center">
-
-            <section class="d-flex align-items-center">
-                <img class="img-fluid" src="../assets/img/buy-comics-digital-comics.png" alt="">
-                <span>digital comics</span>
-            </section>
-
-            <section class="d-flex align-items-center">
-                <img class="img-fluid" src="../assets/img/buy-comics-merchandise.png" alt="">
-                <span>dc merchandise</span>
-            </section>
-
-            <section class="d-flex align-items-center">
-                <img class="img-fluid" src="../assets/img/buy-comics-subscriptions.png" alt="">
-                <span>subscription</span>
-            </section>
-
-            <section class="d-flex align-items-center">
-                <img class="img-fluid" src="../assets/img/buy-comics-shop-locator.png" alt="">
-                <span>comic schop locator</span>
-            </section>
-
-            <section class="d-flex align-items-center">
-                <img class="img-fluid" src="../assets/img//buy-dc-power-visa.svg" alt="">
-                <span>dc power visa</span>
-            </section>
+    
+        <div class="secondary_section ">
+            <div class="py-3 container d-flex justify-content-around align-content-center">
+    
+                <section class="d-flex align-items-center">
+                    <img class="img-fluid" src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>digital comics</span>
+                </section>
+    
+                <section class="d-flex align-items-center">
+                    <img class="img-fluid" src="../assets/img/buy-comics-merchandise.png" alt="">
+                    <span>dc merchandise</span>
+                </section>
+    
+                <section class="d-flex align-items-center">
+                    <img class="img-fluid" src="../assets/img/buy-comics-subscriptions.png" alt="">
+                    <span>subscription</span>
+                </section>
+    
+                <section class="d-flex align-items-center">
+                    <img class="img-fluid" src="../assets/img/buy-comics-shop-locator.png" alt="">
+                    <span>comic shop locator</span>
+                </section>
+    
+                <section class="d-flex align-items-center">
+                    <img class="img-fluid" src="../assets/img//buy-dc-power-visa.svg" alt="">
+                    <span>dc power visa</span>
+                </section>
+            </div>
         </div>
-    </div>
+    </main>
 </template>
 
 
